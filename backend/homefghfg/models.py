7db7211d0699d1e5067f73fc45a7602e48f3459b
Crowdbotics <team@crowdbotics.com>
2020-11-26 05:34:07 +0000

    Update models

---
+++ backend/homefghfg/models.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.db import models
+
+
+class Fghgh(models.Model):
+    "Generated Model"
+    fghfghjf = models.SmallIntegerField()
+
+
+# Create your models here.
